4027c941ba62b709fd04895932a42bf4f8e3f8bd
Jagdeep30 <js6816316@gmail.com>
2024-02-10 23:26:39 +0530

    Time: 12 ms (31.89%) | Memory: 44.9 MB (28.48%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -4,26 +4,36 @@ class Solution {
         boolean[][] dp = new boolean[n][n];
         int ans = 0;
 
-        //length 1 palindromes
-        for(int i=0;i<n;i++){
-            dp[i][i]=true;
-            ans++;
-        }
+        // //length 1 palindromes
+        // for(int i=0;i<n;i++){
+        //     dp[i][i]=true;
+        //     ans++;
+        // }
 
-        //length 2 palindromes
-        for(int i=0;i<=n-2;i++){
-            if(s.charAt(i)==s.charAt(i+1)){
-                dp[i][i+1]=true;
-                ans++;
-            }
-        }
+        // //length 2 palindromes
+        // for(int i=0;i<=n-2;i++){
+        //     if(s.charAt(i)==s.charAt(i+1)){
+        //         dp[i][i+1]=true;
+        //         ans++;
+        //     }
+        // }
 
-        //length greater than 2 palindromes
-        for(int len=3;len<=n;len++){
+        // //length greater than 2 palindromes
+        // for(int len=3;len<=n;len++){
+        //     for(int i=0;i<n-len+1;i++){
+        //         int j = i+len-1;
+        //         if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true){
+        //             dp[i][j]=true;
+        //             ans++;
+        //         }
+        //     }
+        // }
+
+
+        for(int len=1;len<=n;len++){
             for(int i=0;i<n-len+1;i++){
-                int j = i+len-1;
-                if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true){
-                    dp[i][j]=true;
+                if(s.charAt(i)==s.charAt(i+len-1) && (len<=2 || dp[i+1][i+len-2])){
+                    dp[i][i+len-1]=true;
                     ans++;
                 }
             }
