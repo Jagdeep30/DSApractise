5863c7719214fbeaabc2b9927131f07bd9ecc28e
Jagdeep30 <js6816316@gmail.com>
2024-02-14 18:36:09 +0530

    Time: 3 ms (100.00%) | Memory: 76.7 MB (74.37%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,24 +1,37 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> neg = new ArrayList<>();
+        // List<Integer> pos = new ArrayList<>();
+        // List<Integer> neg = new ArrayList<>();
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=0)pos.add(nums[i]);
-            else neg.add(nums[i]);
-        }
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]>=0)pos.add(nums[i]);
+        //     else neg.add(nums[i]);
+        // }
 
+        int n = nums.length;
         int j=0;
-        int k=0;
-        for(int i=0;i<nums.length;i++){
-            if((i&1)==0){
-                nums[i] = pos.get(j++);
+        int k=1;
+        int[] ans = new int[n];
+        // for(int i=0;i<n;i++){
+        //     if((i&1)==0){
+        //         nums[i] = pos.get(j++);
+        //     }
+        //     else{
+        //         nums[i] = neg.get(k++);
+        //     }
+        // }
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0){
+                ans[j] = nums[i];
+                j+=2;
             }
             else{
-                nums[i] = neg.get(k++);
+                ans[k] = nums[i];
+                k+=2;
             }
         }
 
-        return nums;
+        // return nums;
+        return ans;
     }
 }
