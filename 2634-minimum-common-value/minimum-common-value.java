e445b691c1fc7417cb58684fa0073466a9bd1940
Jagdeep30 <js6816316@gmail.com>
2024-03-09 09:37:44 +0530

    Time: 0 ms (100.00%) | Memory: 62.6 MB (47.63%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -5,6 +5,8 @@ class Solution {
         int n = nums1.length;
         int m = nums2.length;
 
+        if(nums1[n-1]<nums2[0] || nums1[0]>nums2[m-1])return -1;
+
         while(i<n && j<m){
             if(nums1[i]==nums2[j])return nums1[i];
             else if(nums1[i]<nums2[j]){
