8400be0bd81d9c7367e20d09d4c27d8885f37ea5
Jagdeep30 <js6816316@gmail.com>
2024-03-10 10:48:15 +0530

    Time: 3 ms (53.96%) | Memory: 43.1 MB (61.49%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -13,35 +13,55 @@ class Solution {
 
         // return ans;
 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        /////////////////////////////////////////////////////////////////////////
 
-        System.out.println(Arrays.toString(nums1));
-        System.out.println(Arrays.toString(nums2));
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
 
-        List<Integer> st = new ArrayList<>();
-        int i=0;
-        int j=0;
-        int n=nums1.length;
-        int m=nums2.length;
+        // System.out.println(Arrays.toString(nums1));
+        // System.out.println(Arrays.toString(nums2));
 
-        while(i<n && j<m){
-            if(nums1[i]==nums2[j]){
-                st.add(nums1[i]);
-                i++;j++;
-                while(i<n && nums1[i]==nums1[i-1])i++;
-                while(j<m && nums2[j]==nums2[j-1])j++;
-            }
-            else if(nums1[i]<nums2[j]){
-                i++;
-            }
-            else j++;
+        // List<Integer> st = new ArrayList<>();
+        // int i=0;
+        // int j=0;
+        // int n=nums1.length;
+        // int m=nums2.length;
+
+        // while(i<n && j<m){
+        //     if(nums1[i]==nums2[j]){
+        //         st.add(nums1[i]);
+        //         i++;j++;
+        //         while(i<n && nums1[i]==nums1[i-1])i++;
+        //         while(j<m && nums2[j]==nums2[j-1])j++;
+        //     }
+        //     else if(nums1[i]<nums2[j]){
+        //         i++;
+        //     }
+        //     else j++;
+        // }
+
+        // int[] ans = new int[st.size()];
+        // // System.out.println(st);
+        // i=0;
+        // for(int num:st)ans[i++]=num;
+        // return ans;
+
+        /////////////////////////////////////////////////////////////////////
+
+        Map<Integer,Boolean> mp = new HashMap<>();
+        Set<Integer> arr = new HashSet<>();
+        for(int i:nums1){
+            mp.put(i,true);
         }
 
-        int[] ans = new int[st.size()];
-        // System.out.println(st);
-        i=0;
-        for(int num:st)ans[i++]=num;
+        for(int i:nums2){
+            if(mp.getOrDefault(i,false))arr.add(i);
+        }
+
+        int[] ans = new int[arr.size()];
+        int i=0;
+        for(int num:arr)ans[i++]=num;
+
         return ans;
     }
 }
