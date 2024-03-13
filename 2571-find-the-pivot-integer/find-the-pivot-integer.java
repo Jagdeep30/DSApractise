89cf9ece0b8de7830b49c13c2e50fa8f5dc66d80
Jagdeep30 <js6816316@gmail.com>
2024-03-13 17:58:56 +0530

    Time: 1 ms (77.54%) | Memory: 40.3 MB (78.99%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,24 +1,38 @@
 class Solution {
     public int pivotInteger(int n) {
-        int[] prefix = new int[n+1];
-        int[] suffix = new int[n+1];
+        // int[] prefix = new int[n+1];
+        // int[] suffix = new int[n+1];
+
+        // for(int i=1;i<=n;i++){
+        //     prefix[i] = prefix[i-1]+i;
+        // }
+
+        // suffix[n] = n;
+        // for(int i=n-1;i>0;i--){
+        //     suffix[i] = suffix[i+1]+i;
+        // }
+
+        // // System.out.println(Arrays.toString(prefix));
+        // // System.out.println(Arrays.toString(suffix));
+
+        // for(int i=1;i<=n;i++){
+        //     if(prefix[i]==suffix[i])return i;
+        // }
+
+        // return -1;
+
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+
+        int currSum = 0;
 
         for(int i=1;i<=n;i++){
-            prefix[i] = prefix[i-1]+i;
+            currSum+=i;
+            if(currSum==sum)return i;
+            sum-=i;
         }
-
-        suffix[n] = n;
-        for(int i=n-1;i>0;i--){
-            suffix[i] = suffix[i+1]+i;
-        }
-
-        // System.out.println(Arrays.toString(prefix));
-        // System.out.println(Arrays.toString(suffix));
-
-        for(int i=1;i<=n;i++){
-            if(prefix[i]==suffix[i])return i;
-        }
-
         return -1;
     }
 }
