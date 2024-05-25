604409f4c3a174781a131ac1f17d80ab201c3dc5
Jagdeep30 <js6816316@gmail.com>
2024-05-25 19:08:37 +0530

    Time: 5 ms (33.46%) | Memory: 41.5 MB (68.54%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,10 +1,14 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
+        HashMap<Integer, List<String>> hm = new HashMap<>();
         HashSet<String> hs = new HashSet<>(wordDict);
-        return wordBreakHelper(s, 0, hs);
+        return wordBreakHelper(s, 0, hs, hm);
     }
 
-    private List<String> wordBreakHelper(String s, int start, HashSet<String> dict) {
+    private List<String> wordBreakHelper(String s, int start, HashSet<String> dict, HashMap<Integer, List<String>> hm) {
+        if (hm.containsKey(start))
+            return hm.get(start);
+
         List<String> validSubstr = new ArrayList<>();
 
         if (start == s.length())
@@ -12,12 +16,14 @@ class Solution {
         for (int end = start + 1; end <= s.length(); end++) {
             String prefix = s.substring(start, end);
             if (dict.contains(prefix)) {
-                List<String> suffixes = wordBreakHelper(s, end, dict);
+                List<String> suffixes = wordBreakHelper(s, end, dict, hm);
                 for (String suffix : suffixes) {
                     validSubstr.add(prefix + (suffix.equals("") ? "" : " ") + suffix);
                 }
             }
         }
+
+        hm.put(start, validSubstr);
         return validSubstr;
     }
 }
