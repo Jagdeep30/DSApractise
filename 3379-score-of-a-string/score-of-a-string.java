622f96b898395b0f07ec8789061979985372ab1a
Jagdeep30 <js6816316@gmail.com>
2024-06-01 19:13:49 +0530

    Time: 1 ms (99.87%) | Memory: 41.9 MB (60.00%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -2,11 +2,7 @@ class Solution {
     public int scoreOfString(String s) {
         char[] arr = s.toCharArray();
         int ans = 0;
-
-        for(int i=0;i<arr.length-1;i++){
-            ans += Math.abs(arr[i] - arr[i+1]);
-        }
-
+        for(int i=0;i<arr.length-1;i++)ans += Math.abs(arr[i] - arr[i+1]);
         return ans;
     }
 }
