4b84dc79925df1dabff80f9f6acf31319b961588
Jagdeep30 <js6816316@gmail.com>
2024-10-12 12:02:09 +0530

    Time: 21 ms (30.84%), Space: 57.8 MB (50.62%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,28 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer,List<Integer>> mp = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            List<Integer> arr = mp.getOrDefault(nums[i],new ArrayList<Integer>());
-            arr.add(i);
-            mp.put(nums[i],arr);
-        }
+        // Map<Integer,List<Integer>> mp = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     List<Integer> arr = mp.getOrDefault(nums[i],new ArrayList<Integer>());
+        //     arr.add(i);
+        //     mp.put(nums[i],arr);
+        // }
 
-        for(Map.Entry<Integer,List<Integer>> e:mp.entrySet()){
-            List<Integer> arr = e.getValue();
-            if(arr.size()<2)continue;
-            for(int i=0;i<arr.size()-1;i++){
-                if(arr.get(i+1)-arr.get(i)<=k)return true;
+        // for(Map.Entry<Integer,List<Integer>> e:mp.entrySet()){
+        //     List<Integer> arr = e.getValue();
+        //     if(arr.size()<2)continue;
+        //     for(int i=0;i<arr.size()-1;i++){
+        //         if(arr.get(i+1)-arr.get(i)<=k)return true;
+        //     }
+        // }
+        // return false;
+
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int ind = mp.getOrDefault(nums[i],-1);
+            if(ind!=-1){
+                if(i-ind<=k)return true;
             }
+            mp.put(nums[i],i);
         }
         return false;
     }
