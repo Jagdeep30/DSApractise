b55b3a414b12c077651aa3cff013a5b198a31efd
Jagdeep30 <js6816316@gmail.com>
2024-10-24 22:47:50 +0530

    Time: 18 ms (79.7%), Space: 94.7 MB (30.61%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ls = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            ls.add(i);
+            find(i,k-1,n,ls,ans);
+            ls.remove(0);
+        }
+        return ans;
+    }
+    static void find(int i, int k, int n, List<Integer> ls, List<List<Integer>> ans){
+        if(k==0){
+            ans.add(new ArrayList<Integer>(ls));
+            return;
+        }
+
+        for(int j=i+1;j<=n;j++){
+            ls.add(j);
+            find(j,k-1,n,ls,ans);
+            ls.remove(ls.size()-1);
+        }
+        return;
+    }
+}
