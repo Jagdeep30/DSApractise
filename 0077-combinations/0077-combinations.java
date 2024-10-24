66e2fc17fce3393b81cde18065594140a3fccbf9
Jagdeep30 <js6816316@gmail.com>
2024-10-24 22:50:21 +0530

    Time: 17 ms (87.24%), Space: 94.4 MB (60.58%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> output = new ArrayList<>();
-        solve(ans,output,n,k,1);
+        List<Integer> ls = new ArrayList<>();
+        // for(int i=1;i<=n;i++){
+            // ls.add(i);
+            find(0,k,n,ls,ans);
+            // ls.remove(0);
+        // }
         return ans;
     }
-
-    static void solve(List<List<Integer>> ans, List<Integer> output, int n, int k, int index){
-        //base case
+    static void find(int i, int k, int n, List<Integer> ls, List<List<Integer>> ans){
         if(k==0){
-            List<Integer> temp = new ArrayList<>(output);
-            ans.add(temp);
+            ans.add(new ArrayList<Integer>(ls));
             return;
         }
 
-        //processing
-        for(int i = index;i<=n;i++){
-            output.add(i);
-            // System.out.print(output);
-            solve(ans,output,n,k-1,i+1);
-            output.remove(output.size()-1);
-            // System.out.println();
+        for(int j=i+1;j<=n;j++){
+            ls.add(j);
+            find(j,k-1,n,ls,ans);
+            ls.remove(ls.size()-1);
         }
-        return ;
+        return;
     }
 }
