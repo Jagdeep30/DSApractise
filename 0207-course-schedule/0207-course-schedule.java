f041ec1a7a757bb8917a10f1ba04bff370f9109f
Jagdeep30 <js6816316@gmail.com>
2024-10-27 12:16:06 +0530

    Time: 1001 ms (5.01%), Space: 46.2 MB (7.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,54 +2,38 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] p) {
         Map<Integer,List<Integer>> mp = new HashMap<>();
         for(int i=0;i<p.length;i++){
-            // if(p[i][0]==p[i][1])continue;
             List<Integer> ls  = mp.getOrDefault(p[i][0],new ArrayList<Integer>());
-            // mp.put(p[i][0],p[i][1]);
             ls.add(p[i][1]);
             mp.put(p[i][0],ls);
         }
         boolean ans = true;
         Map<Integer,Boolean> res = new HashMap<>();
         for(int i=0;i<numCourses;i++){
-            if(res.getOrDefault(i,false)){
-                ans = ans && res.get(i);
+            if(!res.getOrDefault(i,false)){
+                res.put(i,dfs(i,numCourses,mp,new HashMap<>(),res));
                 // System.out.println("ans for "+i+" are "+res.get(i));
-                continue;
             }
-            boolean r = dfs(i,numCourses,mp,new HashMap<>(),res);
-            ans = ans && r;
+            ans = ans && res.get(i);
             // System.out.println("ans for "+i+" is "+r);
-            res.put(i,r);
         }
         return ans;
     }
 
     static boolean dfs(int i, int numCourses, Map<Integer,List<Integer>> mp, Map<Integer,Boolean> visited, Map<Integer,Boolean> r){
-        // System.out.println("reached");
         if(mp.getOrDefault(i,null)==null)return true;
-        if(i>=numCourses)return true;
         if(r.getOrDefault(i,false))return true;
-// System.out.println(i);
         visited.put(i,true);
         List<Integer> ls = mp.get(i);
-        if(ls==null)return true;
         boolean ans = true;
         for(int j=0;j<ls.size();j++){
-            if(!visited.getOrDefault(ls.get(j),false)){
-                if(r.getOrDefault(ls.get(j),false)){
-                    ans = ans && r.get(ls.get(j));
-                    // System.out.println("ans for "+j+" are "+r.get(ls.get(j)));
-                    continue;
+            int num = ls.get(j);
+            if(!visited.getOrDefault(num,false)){
+                if(!r.getOrDefault(num,false)){
+                    r.put(num,dfs(num,numCourses,mp,visited,r));
                 }
-                boolean rr = dfs(ls.get(j),numCourses,mp,visited,r);
-                ans = ans && rr;
-                r.put(ls.get(j),rr);
-                // System.out.println("ans for "+j+" is "+rr);
             }
-            else ans = r.getOrDefault(ls.get(j),false);
+            ans = ans && r.getOrDefault(num,false);
         }
-        // visited.put(i,false);
-        // System.out.println("end for "+i+" were "+ans);
         r.put(i,ans);
         return ans;
     }
