409a741ef42f41311dba8523eff753c5f2d1a01c
Jagdeep30 <js6816316@gmail.com>
2024-11-03 22:16:09 +0530

    Time: 15 ms (6.13%), Space: 44.7 MB (22.3%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Map<String,Boolean> mp = new HashMap<>();
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp,3);
+        for(String i:wordDict){
+            mp.put(i,true);
+        }
+        return solve(0,s,mp,dp);
+    }
+    static boolean solve(int i, String s, Map<String,Boolean> mp, int[] dp){
+        //base case
+        if(i==s.length())return true;
+
+        if(dp[i]==1)return true;
+        if(dp[i]==0)return false;
+
+        String temp = "";
+        boolean ans = false;
+        for(int j=i;j<s.length();j++){
+            temp+=s.charAt(j);
+            if(mp.getOrDefault(temp,false)){
+                ans = ans || solve(j+1,s,mp,dp);
+            }
+        }
+        if(mp.getOrDefault(temp,false))ans = true;
+        dp[i] = ans?1:0;
+        return ans;
+    }
+}
