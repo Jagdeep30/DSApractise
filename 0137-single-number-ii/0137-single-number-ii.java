cd7c07ae16f5d0420a63d4e7e2946db0464728b5
Jagdeep30 <js6816316@gmail.com>
2024-11-03 18:52:20 +0530

    Time: 4 ms (59.91%), Space: 45.6 MB (31.08%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        String[] ls = new String[nums.length];
-        for(int i=0;i<nums.length;i++){
-            ls[i] = Integer.toBinaryString(nums[i]);
-        }
+        //APPROACH 1
+        // int ans = 0;
+        // for(int i=0;i<32;i++){
+        //     int c = 0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if((nums[j] & (1<<i))!=0)c++;
+        //     }
+        //     if(c%3!=0)ans |= (1<<i);
+        // }
+        // return ans;
 
-        String ans = "";
-        for(int i=0;i<32;i++){
-            int c = 0;
-            for(int j=0;j<ls.length;j++){
-                int ind = ls[j].length()-i-1;
-                if(ind>=0 && ls[j].charAt(ind)=='1'){
-                    c++;
-                }
-            }
-            if(c%3!=0)ans = "1"+ans;
-            else ans = "0" + ans;
+        //APPROACH 2
+        Arrays.sort(nums);
+        int i=1;
+        while(i<nums.length){
+            if(nums[i]!=nums[i-1])return nums[i-1];
+            i+=3;
         }
-        return Integer.parseInt(ans,2);
+        return nums[nums.length-1];
     }
 }
