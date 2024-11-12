f72fd7f23c5d18dbdaa8a3e15272b0ef15794cee
Jagdeep30 <js6816316@gmail.com>
2024-11-12 19:57:19 +0530

    Time: 12 ms (26.57%), Space: 45 MB (21.92%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int calculate(String s) {
+        int[] res = solve(0,s);
+        return res[1];
+    }
+
+    static int[] solve( int i, String s){
+        int res = 0;
+        int num = 0;
+        int sign = 1;
+        while(i<s.length()){
+            char c = s.charAt(i);
+            if(c>='0' && c<='9'){
+                num = (num * 10) + (c-'0');
+            }
+            else if(c=='('){
+                int[] ans = solve(i+1,s);
+                System.out.println(Arrays.toString(ans));
+                i = ans[0];
+                i--;
+                res += (ans[1]*sign);
+            }
+            else if(c==')'){
+                //return the result
+                res += (num*sign);
+                return new int[]{i+1,res};
+            }
+            else if(c=='+' || c=='-'){
+                res += (sign*num);
+                num = 0;
+                if(c=='+')sign = 1;
+                else if(c=='-')sign = -1;
+            }
+            i++;
+        }
+        res += (num*sign);
+        return new int[]{i+1,res};
+    }
+}
