fc1d607d070a4ce766c20e984e1a61c667ecf3ba
Jagdeep30 <js6816316@gmail.com>
2024-12-08 12:05:00 +0530

    Time: 38 ms (17.89%), Space: 54.8 MB (70.19%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,14 +1,29 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        mp.put(0,1);
-        int sum = 0;
-        int ans = 0;
-        for(int i=0;i<nums.length;i++){
-            sum+=(nums[i]%2==0?0:1);
-            ans += mp.getOrDefault(sum-k,0);
-            mp.put(sum,mp.getOrDefault(sum,0)+1);
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if((nums[i]&1)==0){
+                nums[i]=0;
+            }
+            else{
+                nums[i]=1;
+            }
         }
+        Map<Integer,Integer> mp = new HashMap<>();
+        mp.put(0,1);
+        int[] ps = new int[n+1];
+
+        for(int i=0;i<n;i++){
+            ps[i+1] = ps[i] + nums[i];
+            mp.put(ps[i+1],mp.getOrDefault(ps[i+1],0)+1);
+        }
+
+        int ans = 0;
+        for(int i=0;i<=n;i++){
+            int req = ps[i]+k;
+            ans+=mp.getOrDefault(req,0);
+        }
+
         return ans;
     }
 }
