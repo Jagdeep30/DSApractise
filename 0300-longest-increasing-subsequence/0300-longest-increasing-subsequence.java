70154e06ae30ddab3b1a6e9c0923c60e4c698a42
Jagdeep30 <js6816316@gmail.com>
2025-01-12 11:47:36 +0530

    Time: 2 ms (99.62%), Space: 44.5 MB (35.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,8 +1,27 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length];
-        for(int i=0;i<dp.length;i++)Arrays.fill(dp[i],-1);
-        return solve(0,nums,-1,dp);
+        // int[][] dp = new int[nums.length][nums.length];
+        // for(int i=0;i<dp.length;i++)Arrays.fill(dp[i],-1);
+        // return solve(0,nums,-1,dp);
+
+        int[] temp = new int[nums.length];
+        int size = 0;
+        for(int i=0;i<nums.length;i++){
+            int ind = binaryS(temp, size, nums[i]);
+            if(ind == size)size++;
+        }
+        return size;
+    }
+    static int binaryS(int[] temp, int size, int target){
+        int s = 0;
+        int e = size;
+        while(s<e){
+            int mid = s+(e-s)/2;
+            if(temp[mid]>=target)e = mid;
+            else s = mid + 1;
+        }
+        temp[e] = target;
+        return e;
     }
 
     static int solve(int i, int[] nums, int prev, int[][] dp){
