b8a0c21ee6f17de8c6eb882c5cb8922705cb3b96
Jagdeep30 <js6816316@gmail.com>
2025-01-12 12:36:14 +0530

    Time: 12 ms (37.33%), Space: 46.1 MB (43.46%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,26 +1,47 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int n = arr.length;
-        int s = 0;
-        int e = n-1;
-        int temp = -1;
-        if(x<arr[0])temp = 0;
-        else if(x>arr[n-1])temp = n-k;
-        else {    
-            while(s<e){
-                int mid = s+(e-s)/2;
-                if(arr[mid]<x)s = mid + 1;
-                else e = mid;
-            }
-
-            temp = Math.max(0,e-(k/2));
-        }
-
         List<Integer> ls = new ArrayList<>();
-        for(int i=0;i<k;i++){
-            ls.add(arr[temp++]);
-        }
-        return ls;
+        int ind = binaryS(arr, x);
+        int l = ind-1;
+        int r = ind;
+        int c = 0;
 
+        while(l>=0 && r<arr.length && c<k){
+            int d1 = Math.abs(arr[l]-x);
+            int d2 = Math.abs(arr[r]-x);
+            if(d1<=d2){
+                ls.add(arr[l]);
+                l--;
+                c++;
+            }
+            else{
+                ls.add(arr[r]);
+                c++;
+                r++;
+            }
+        }
+        while(c<k && l>=0){
+            ls.add(arr[l]);
+            c++;l--;
+        }
+        while(c<k && r<arr.length){
+            ls.add(arr[r]);
+            c++;r++;
+        }
+
+        Collections.sort(ls);
+        return ls;
     }
+
+    static int binaryS(int[] arr, int target){
+        int s = 0;
+        int e = arr.length-1;
+        while(s<e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]>=target)e = mid;
+            else s = mid + 1;
+        }
+        return e;
+    }
+    
 }
