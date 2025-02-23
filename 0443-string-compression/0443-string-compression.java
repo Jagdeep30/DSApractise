95f9a83f785b227e0d0793f34b1dee7d752d40a1
Jagdeep30 <js6816316@gmail.com>
2025-02-23 21:32:06 +0530

    Time: 1 ms (99.66%), Space: 43.9 MB (79.3%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,24 +1,23 @@
 class Solution {
     public int compress(char[] chars) {
-        int ans = 0;
         int count = 0;
         int i=0;
         int n = chars.length;
         int index = 0;
         while(i<n){
-            int ind = countC(chars,i);
+            int ind = countC(chars,i,n);
             chars[index] = chars[i];
             count++;
             if(ind-i>1){
                 count+=Integer.toString(ind-i).length();
-                index+=editArray(chars, ind-i, index);
+                index+=editArray(chars, ind-i, index+1);
             }
             index++;
             // System.out.println(index);
 
             i=ind;
         }
-        System.out.println(index);
+        // System.out.println(index);
         // count+=Integer.toString(ind-i).length();
         // chars[index] = chars[i];
         // index+=editArray(chars, ind-i, index);
@@ -26,16 +25,15 @@ class Solution {
         return count;
     }
 
-    static int countC(char[] chars, int i){
+    static int countC(char[] chars, int i, int n){
         int j = i+1;
         char target = chars[i];
-        while(j<chars.length && chars[j]==target)j++;
+        while(j<n && chars[j]==target)j++;
         return j;
     }
     static int editArray(char[] chars, int c, int i){
         String count = Integer.toString(c);
-        i++;
-            System.out.println(i);
+            // System.out.println(i);
         for(int j=0;j<count.length();j++){
             // System.out.println(Arrays.toString(chars));
             chars[i+j] = count.charAt(j);
