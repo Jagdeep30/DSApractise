ba48e6fae158611f0cddcc0b22890d08e9908905
Jagdeep30 <js6816316@gmail.com>
2025-03-08 14:36:14 +0530

    Time: 14 ms (52.63%), Space: 45.9 MB (36.44%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,31 +1,32 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        // lenght equal
+        // same characters
+        // same frequency numbers
 
-        for (char ch : word1.toCharArray()) {
-            freq1[ch - 'a']++;
+        if(word1.length() != word2.length())return false;
+        int[] f1 = new int[26];
+        int[] f2 = new int[26];
+
+        for(int i=0;i<word1.length();i++){
+            f1[word1.charAt(i) - 'a']++;
         }
-
-        for (char ch : word2.toCharArray()) {
-            freq2[ch - 'a']++;
+        for(int i=0;i<word2.length();i++){
+            f2[word2.charAt(i) - 'a']++;
         }
+        System.out.println(Arrays.toString(f1));
+        System.out.println(Arrays.toString(f2));
 
-        for (int i = 0; i < 26; i++) {
-            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
-                return false;
-            }
+        for(int i=0;i<26;i++){
+            if(f1[i]!=0 && f2[i]==0 || f1[i]==0 && f2[i]!=0)return false;
         }
+        Arrays.sort(f1);
+        Arrays.sort(f2);
 
-        Arrays.sort(freq1);
-        Arrays.sort(freq2);
 
-        for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) {
-                return false;
-            }
+        for(int i=0;i<26;i++){
+            if(f1[i]!=f2[i])return false;
         }
-
         return true;
     }
 }
