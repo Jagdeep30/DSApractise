7e93e47e61823f0a2f14e345231afd987a50f6ca
Jagdeep30 <js6816316@gmail.com>
2025-03-08 14:32:20 +0530

    Time: 10 ms (74.44%), Space: 45.6 MB (54.31%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,36 +1,26 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        Map<Integer,List<Integer>> mp = new HashMap<>();
+        Map<Integer,Boolean> mp1 = new HashMap<>();
+        Map<Integer,Boolean> mp2 = new HashMap<>();
         for(int i=0;i<nums1.length;i++){
-            List<Integer> p = mp.getOrDefault(nums1[i], new ArrayList<Integer>());
-            p.add(1);
-            p.add(0);
-            mp.put(nums1[i],p);
+            mp1.put(nums1[i], true);
         }
-
         for(int i=0;i<nums2.length;i++){
-            List<Integer> p = mp.getOrDefault(nums2[i], new ArrayList<Integer>());
-            if(p.size()==0){
-                p.add(0);
-                p.add(1);
-            }
-            else p.set(1,1);
-
-            mp.put(nums2[i], p);
+            mp2.put(nums2[i], true);
         }
 
         List<List<Integer>> ans = new ArrayList<>();
         ans.add(new ArrayList<Integer>());
         ans.add(new ArrayList<Integer>());
 
-        for(Map.Entry<Integer,List<Integer>> e: mp.entrySet()){
-            int val = e.getKey();
-            List<Integer> temp = e.getValue();
-            if(temp.get(0)==1 && temp.get(1)==0){
-                ans.get(0).add(val);
+        for(Map.Entry<Integer,Boolean> e : mp1.entrySet()){
+            if(!mp2.getOrDefault(e.getKey(), false)){
+                ans.get(0).add(e.getKey());
             }
-            if(temp.get(1)==1 && temp.get(0)==0){
-                ans.get(1).add(val);
+        }
+        for(Map.Entry<Integer,Boolean> e : mp2.entrySet()){
+            if(!mp1.getOrDefault(e.getKey(), false)){
+                ans.get(1).add(e.getKey());
             }
         }
         return ans;
