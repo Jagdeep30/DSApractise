c8c0c07652729e455d149ad437360e9b6ffd7a22
Jagdeep30 <js6816316@gmail.com>
2025-03-08 14:23:17 +0530

    Time: 11 ms (86.86%), Space: 44.7 MB (56.56%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,24 +1,39 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        String temp = "";
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')temp+='1';
-            else temp+='0';
-        }
-
+        // String temp = "";
         int sum = 0;
         int ans = 0;
-        // System.out.println(temp);
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')temp+='1';
+        //     else temp+='0';
+        // }
+
         for(int i=0;i<k;i++){
-            if(temp.charAt(i)=='1')sum++;
+            char ch = s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')sum+=1;
+            // else temp+='0';
         }
         ans = Math.max(ans,sum);
-        for(int i=k;i<temp.length();i++){
-            sum += temp.charAt(i)=='1'?1:0;
-            sum -= temp.charAt(i-k)=='1'?1:0;
+        for(int i=k;i<s.length();i++){
+            char ch = s.charAt(i);
+            char chh = s.charAt(i-k);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')sum++;
+            if(chh=='a' || chh=='e' || chh=='i' || chh=='o' || chh=='u')sum--;
             ans = Math.max(ans,sum);
+            // else temp+='0';
         }
+
+        // System.out.println(temp);
+        // for(int i=0;i<k;i++){
+        //     if(temp.charAt(i)=='1')sum++;
+        // }
+        // ans = Math.max(ans,sum);
+        // for(int i=k;i<temp.length();i++){
+        //     sum += temp.charAt(i)=='1'?1:0;
+        //     sum -= temp.charAt(i-k)=='1'?1:0;
+        //     ans = Math.max(ans,sum);
+        // }
         return ans;
     }
 }
