db823465b69ca0993014d1f4188e45ce9c005b83
Jagdeep30 <js6816316@gmail.com>
2025-03-11 19:10:27 +0530

    Time: 5 ms (30.76%), Space: 42.5 MB (28.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,30 +1,32 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        if(digits.equals(""))return new ArrayList<String>();
-        Map<Character,List<Character>> mp = new HashMap<>();
-        mp.put('2',new ArrayList<Character>(Arrays.asList('a','b','c')));
-        mp.put('3',new ArrayList<Character>(Arrays.asList('d','e','f')));
-        mp.put('4',new ArrayList<Character>(Arrays.asList('g','h','i')));
-        mp.put('5',new ArrayList<Character>(Arrays.asList('j','k','l')));
-        mp.put('6',new ArrayList<Character>(Arrays.asList('m','n','o')));
-        mp.put('7',new ArrayList<Character>(Arrays.asList('p','q','r','s')));
-        mp.put('8',new ArrayList<Character>(Arrays.asList('t','u','v')));
-        mp.put('9',new ArrayList<Character>(Arrays.asList('w','x','y','z')));
-
+        Map<Character,String> mp = new HashMap<>();
         List<String> ans = new ArrayList<>();
+        if(digits.isEmpty())return ans;
+        mp.put('2',"abc");
+        mp.put('3',"def");
+        mp.put('4',"ghi");
+        mp.put('5',"jkl");
+        mp.put('6',"mno");
+        mp.put('7',"pqrs");
+        mp.put('8',"tuv");
+        mp.put('9',"wxyz");
+
         solve(0,digits,mp,ans,"");
         return ans;
     }
 
-    static void solve(int i, String d, Map<Character,List<Character>> mp, List<String> ans, String curr){
-        if(i==d.length()){
-            ans.add(curr);
+    static void solve(int i, String d, Map<Character,String> mp, List<String> ans, String out){
+        if(i>=d.length()){
+            ans.add(new String(out));
             return;
         }
-        List<Character> ls = mp.get(d.charAt(i));
-        for(int j=0;j<ls.size();j++){
-            solve(i+1,d,mp,ans,curr+ls.get(j));
+
+        char[] temp = mp.get(d.charAt(i)).toCharArray();
+        for(int j=0;j<temp.length;j++){
+            solve(i+1,d,mp,ans,out+temp[j]);
         }
+
         return;
     }
 }
