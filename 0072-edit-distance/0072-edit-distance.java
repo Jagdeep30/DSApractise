0a191ca5524fefaba03aaed6cc4cf47d517828fb
Jagdeep30 <js6816316@gmail.com>
2025-03-17 22:52:41 +0530

    Time: 5 ms (64.39%), Space: 45.5 MB (7.27%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,33 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int[][] dp = new int[word1.length()][word2.length()];
-        for(int i=0;i<dp.length;i++)Arrays.fill(dp[i],-1);
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
         return solve(0,0,word1,word2,dp);
     }
 
     static int solve(int i, int j, String word1, String word2, int[][] dp){
+        //base case
+        if(i==word1.length() && j==word2.length())return 0;
+        if(i==word1.length()){
+            return word2.length()-j;
+        }
         if(j==word2.length())return word1.length()-i;
-        if(i==word1.length())return word2.length()-j;
 
         if(dp[i][j]!=-1)return dp[i][j];
-
-        int min = 1000000;
-        if(word1.charAt(i)==word2.charAt(j))min = Math.min(min,solve(i+1,j+1,word1,word2,dp));
-        else{
+        
+        int min = Integer.MAX_VALUE;
+        //choices
+        if(word1.charAt(i)==word2.charAt(j))return solve(i+1,j+1,word1,word2,dp);
             //insert
-            min = Math.min(min,solve(i,j+1,word1,word2,dp)+1);
+            min = Math.min(min,solve(i,j+1,word1,word2,dp));
             //delete
-            min = Math.min(min,solve(i+1,j,word1,word2,dp)+1);
+            min = Math.min(min,solve(i+1,j,word1,word2,dp));
             //replace
-            min = Math.min(min,solve(i+1,j+1,word1,word2,dp)+1);
-        }
+            min = Math.min(min,solve(i+1,j+1,word1,word2,dp));
 
-        return dp[i][j] = min;
+        //return min value
+        return dp[i][j] = min+1;
     }
 }
