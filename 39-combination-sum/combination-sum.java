fbdb707afb15db18333588af7abbeaa1559a24eb
Jagdeep30 <jagdeepsingh1630@gmail.com>
2025-04-30 21:48:53 +0530

    Time: 10 ms (5.13%) | Memory: 44.9 MB (21.06%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,31 +1,32 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // Set<List<Integer>> ans = new HashSet<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        solve(0,target,candidates,candidates.length,new ArrayList<>(), ans);
-        return ans;
+        // int[][] dp = new int[candidates.length][target+1];
+        // for(int i=0;i<dp.length;i++)Arrays.fill(dp[i], -1);
+        Set<List<Integer>> output = new HashSet<>();
+        solve(0,target,new ArrayList<Integer>(),output,candidates);
+        return new ArrayList<List<Integer>>(output);
     }
 
-    static void solve(int i, int target, int[] candidates, int n, List<Integer> output, List<List<Integer>> ans){
+    static void solve(int i, int target, List<Integer> ls, Set<List<Integer>> output, int[] candidates){
+        //base cases
         if(target==0){
-            if(!ans.contains(output))
-            ans.add(new ArrayList<>(output));
+            output.add(new ArrayList<Integer>(ls));
             return;
         }
-        if(i==n){
-            // if(!ans.contains(output))
-            // ans.add(new ArrayList<>(output));
-            return;
-        }
-        if(target<0)return;
+        if(target<0 || i==candidates.length)return;
 
-        //exclude
-        solve(i+1,target,candidates,n,output,ans);
-        //include
-        output.add(candidates[i]);
-        solve(i,target-candidates[i],candidates,n,output,ans);
-        output.remove(new Integer(candidates[i]));
+        // if(dp[i][target]!=-1)return dp[i][target];
+        //choose
+        ls.add(candidates[i]);
+            //choose same again
+            solve(i,target-candidates[i],ls,output,candidates);
+            //move to next
+            solve(i+1,target-candidates[i],ls,output,candidates);
+        ls.remove(ls.size()-1);
+        //not choose
+        solve(i+1,target,ls,output,candidates);
 
+        //ans
         return;
     }
 }
