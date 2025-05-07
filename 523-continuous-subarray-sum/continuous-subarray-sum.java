d6c35313ccc45459b98f49f4c96c3d494015ff84
Jagdeep30 <jagdeepsingh1630@gmail.com>
2025-05-07 23:55:35 +0530

    Time: 32 ms (7.57%) | Memory: 62.2 MB (23.53%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        int n = nums.length;
-        Map<Integer, Integer> remainderMap = new HashMap<>();
-        int cumulativeSum = 0;
+        Map<Integer,Integer> mp = new HashMap<>();
+        mp.put(0,-1);
         
-        // Step 1: Initialize Remainder Map
-        remainderMap.put(0, -1);
-        
-        // Step 2: Iterate Through the List
-        for (int i = 0; i < n; i++) {
-            // Step 3: Calculate Cumulative Sum
-            cumulativeSum += nums[i];
-            
-            // Step 4: Calculate Remainder
-            int remainder = k == 0 ? cumulativeSum : cumulativeSum % k;
-            
-            // Step 5: Update Remainder Map
-            if (remainderMap.containsKey(remainder)) {
-                // Check if segment length is at least two
-                if (i - remainderMap.get(remainder) > 1) {
-                    return true;
-                }
-            } else {
-                remainderMap.put(remainder, i);
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            int temp = mp.getOrDefault(sum%k,100001);
+
+            if(temp!=100001 && i-temp>1){
+                return true;
             }
+
+            if(temp==100001)mp.put(sum%k,i);
         }
-        
-        // Step 6: Return the Result
+
         return false;
     }
 }
